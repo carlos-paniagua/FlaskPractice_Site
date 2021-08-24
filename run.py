2568a8c9d8bc6d908619c372cde325bd13a9f197
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-08-25 01:26:19 +0900

    add __init__

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from market import app
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# uniform resource identifer
-db = SQLAlchemy(app)
+if __name__ == '__main__':
+    app.run(debug=True)

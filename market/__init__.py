59aff707872b57d123b168f80a933a61cc515fac
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-08-26 02:25:13 +0900

    add login_page

--- market/__init__.py
+++ market/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
@@ -8,5 +9,6 @@ app.config['SECRET_KEY'] = '97d19d0b43958170812a8e2e'
 
 # uniform resource identifer
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from market import routes

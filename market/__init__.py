8ed9f99209ea14a9c847cf96a733a28dc8c43f14
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-08-26 00:24:35 +0900

    edit forms

--- market/__init__.py
+++ market/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '97d19d0b43958170812a8e2e'
 
 # uniform resource identifer
 db = SQLAlchemy(app)

f1c14c782fe4037a9fc05b167b6eacd72e9fd04d
carlos-paniagua <carlos_paniagua0421@hotmail.com>
2021-08-27 11:37:57 +0900

    change budget add font awesome

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FlaskPractice_Site
+# Flask練習サイト
+
+### 参考：JimShapedCoding
+### http://www.jimshapedcoding.com/courses/Flask%20Full%20Series
+### https://www.youtube.com/watch?v=Qr4QMBUPxWo&t=20829s
--- market/__init__.py
+++ market/__init__.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
+# uniform resource identifer
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '97d19d0b43958170812a8e2e'
 
-# uniform resource identifer
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from market import routes
